9c6b9731537d49e0725149ca99e47434d2be43f2
Sébastien Taylor <sebastien@au-zone.com>
2025-06-23 17:12:16 -0600

    Importing Project

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    NixError(nix::Error),
+    NotImplemented(String),
+    InvalidSize(usize),
+    ShapeVolumeMismatch,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IoError(err)
+    }
+}
+
+impl From<nix::Error> for Error {
+    fn from(err: nix::Error) -> Self {
+        Error::NixError(err)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::IoError(e) => write!(f, "IO error: {}", e),
+            Error::NixError(e) => write!(f, "Nix error: {}", e),
+            Error::NotImplemented(msg) => write!(f, "Not implemented: {}", msg),
+            Error::InvalidSize(size) => write!(f, "Invalid size: {}", size),
+            Error::ShapeVolumeMismatch => write!(f, "Shape volume mismatch"),
+        }
+    }
+}
