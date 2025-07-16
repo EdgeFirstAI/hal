735d800d08fe36001ca74933a4e61947cc18e6f0
Sébastien Taylor <sebastien@au-zone.com>
2025-07-15 18:53:43 -0600

    Expanding benchmarks to specifically target various accelerators when available to help compare with baseline.

--- crates/tensor/benches/tensor_benchmark.rs
+++ crates/tensor/benches/tensor_benchmark.rs
@@ -1,41 +1,79 @@
+use std::env;
+
 use num_traits::Num;
 use tensor::{Tensor, TensorMemory, TensorTrait as _};
 
-const MEMORY: &[char] = &[
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
+fn tensor_alloc_mem<T>(size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let _tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Mem), None).expect("Failed to allocate tensor");
+}
+
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
+fn tensor_map_mem<T>(bench: divan::Bencher, size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Mem), None).expect("Failed to allocate tensor");
+    bench.bench_local(|| tensor.map().expect("Failed to map tensor"));
+}
+
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
+fn tensor_alloc_shm<T>(size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let _tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Shm), None).expect("Failed to allocate tensor");
+}
+
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
+fn tensor_map_shm<T>(bench: divan::Bencher, size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Shm), None).expect("Failed to allocate tensor");
+    bench.bench_local(|| tensor.map().expect("Failed to map tensor"));
+}
+
+#[cfg(target_os = "linux")]
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]], ignore = !dma_available())]
+fn tensor_alloc_dma<T>(size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let _tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Dma), None).expect("Failed to allocate tensor");
+}
+
+#[cfg(target_os = "linux")]
+#[divan::bench(types = [u8, f64], args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]], ignore = !dma_available())]
+fn tensor_map_dma<T>(bench: divan::Bencher, size: [usize; 4])
+where
+    T: Num + Clone + Send + Sync + std::fmt::Debug,
+{
+    let tensor =
+        Tensor::<T>::new(&size, Some(TensorMemory::Dma), None).expect("Failed to allocate tensor");
+    bench.bench_local(|| tensor.map().expect("Failed to map tensor"));
+}
+
+fn dma_available() -> bool {
     #[cfg(target_os = "linux")]
-    'D',
-    'S',
-    'M',
-    'N',
-];
-
-#[divan::bench(types = [u8, f64], consts = MEMORY, args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
-fn tensor_alloc<T, const M: char>(size: [usize; 4])
-where
-    T: Num + Clone + Send + Sync + std::fmt::Debug,
-{
-    let _tensor = Tensor::<T>::new(&size, memory::<M>(), None).expect("Failed to allocate tensor");
-}
-
-#[divan::bench(types = [u8, f64], consts = MEMORY, args = [[1, 360, 640, 4], [1, 720, 1280, 4], [1, 1080, 1920, 4], [1, 2160, 3840, 4]])]
-fn tensor_map<T, const M: char>(size: [usize; 4])
-where
-    T: Num + Clone + Send + Sync + std::fmt::Debug,
-{
-    let tensor = Tensor::<T>::new(&size, memory::<M>(), None).expect("Failed to allocate tensor");
-    let _map = tensor.map().expect("Failed to map tensor");
-}
-
-fn memory<const M: char>() -> Option<TensorMemory> {
-    match M {
-        'D' => Some(TensorMemory::Dma),
-        'S' => Some(TensorMemory::Shm),
-        'M' => Some(TensorMemory::Mem),
-        'N' => None,
-        _ => None,
+    {
+        dma_heap::Heap::new(dma_heap::HeapKind::Cma)
+            .or_else(|_| dma_heap::Heap::new(dma_heap::HeapKind::System))
+            .is_ok()
     }
+    #[cfg(not(target_os = "linux"))]
+    false
 }
 
 fn main() {
+    env_logger::init();
     divan::main();
 }
