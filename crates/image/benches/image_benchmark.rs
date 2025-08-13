a7148200e5b89c9fba866834f7d430b228d0b3ce
Charlie-Zheng <charlie@au-zone.com>
2025-08-13 12:35:08 -0600

    Added opengl support for mem buffers

--- crates/image/benches/image_benchmark.rs
+++ crates/image/benches/image_benchmark.rs
@@ -204,8 +204,46 @@ where
 }
 
 #[cfg(target_os = "linux")]
+#[divan::bench(types = [Person, Zidane], args = [(640, 360), (960, 540), (1280, 720), (1920, 1080)])]
+fn resize_opengl_mem<IMAGE>(bencher: divan::Bencher, size: (usize, usize))
+where
+    IMAGE: TestImage,
+{
+    let (width, height) = size;
+    let name = format!("{}.jpg", IMAGE::filename());
+    let path = Path::new("testdata").join(&name);
+    let path = match path.exists() {
+        true => path,
+        false => {
+            let path = Path::new("../testdata").join(&name);
+            if path.exists() {
+                path
+            } else {
+                Path::new("../../testdata").join(&name)
+            }
+        }
+    };
+
+    assert!(path.exists(), "unable to locate test image at {path:?}");
+
+    let file = std::fs::read(path).unwrap();
+
+    let src = TensorImage::load(&file, Some(RGBA), Some(TensorMemory::Mem)).unwrap();
+    let mut gl_dst = TensorImage::new(width, height, RGBA, Some(TensorMemory::Dma)).unwrap();
+    let mut gl_converter =
+        edgefirst_image::GLConverter::new_with_size(width, height, false).unwrap();
+
+    bencher.bench_local(|| {
+        gl_converter
+            .convert(&mut gl_dst, &src, Rotation::None, None)
+            .unwrap()
+    });
+    drop(gl_dst);
+}
+
+#[cfg(target_os = "linux")]
 #[divan::bench(types = [Person, Zidane], args = [(640, 360), (960, 540), (1280, 720), (1920, 1080)], ignore = !dma_available())]
-fn resize_opengl<IMAGE>(bencher: divan::Bencher, size: (usize, usize))
+fn resize_opengl_dma<IMAGE>(bencher: divan::Bencher, size: (usize, usize))
 where
     IMAGE: TestImage,
 {
