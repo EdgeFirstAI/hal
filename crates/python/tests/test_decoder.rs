1681fec10f4ca21440ce28901dfa921797941ada
Charlie-Zheng <charlie@au-zone.com>
2025-09-26 15:34:01 -0600

    Added creating config from python dictionary using pythonize

--- crates/python/tests/test_decoder.rs
+++ crates/python/tests/test_decoder.rs
@@ -129,6 +129,67 @@ fn test_decoder_parse_config_modelpack_split_u8() -> PyResult<()> {
 }
 
 #[test]
+fn test_decoder_parse_config_from_dict() -> PyResult<()> {
+    pyo3::append_to_inittab!(edgefirst_python);
+    pyo3::prepare_freethreaded_python();
+    Python::with_gil(|py| {
+        let out: pyo3::Bound<'_, PyDict> = PyDict::new(py);
+        py.run(
+            c_str!(include_str!("decoder/from_dict.py")),
+            None,
+            Some(&out),
+        )?;
+
+        let py_boxes = out
+            .get_item("boxes")
+            .unwrap()
+            .downcast_into::<PyArray2<f32>>()?;
+
+        let py_scores = out
+            .get_item("scores")
+            .unwrap()
+            .downcast_into::<PyArray1<f32>>()?;
+
+        let py_classes = out
+            .get_item("classes")
+            .unwrap()
+            .downcast_into::<PyArray1<usize>>()?;
+
+        let py_masks = out.get_item("masks").unwrap().downcast_into::<PyList>()?;
+
+        let py_boxes = py_boxes.readonly();
+
+        let boxes = [0.43171933, 0.68243736, 0.5626645, 0.808863];
+
+        assert!(approx::abs_diff_eq!(
+            py_boxes.as_slice()?,
+            boxes.as_slice(),
+            epsilon = 1e-6
+        ));
+
+        let py_scores = py_scores.readonly();
+
+        let scores = [0.49577647];
+
+        assert!(approx::abs_diff_eq!(
+            py_scores.as_slice()?,
+            scores.as_slice(),
+            epsilon = 1e-6
+        ));
+
+        let py_classes = py_classes.readonly();
+
+        let classes = [0];
+
+        assert_eq!(py_classes.as_slice()?, classes.as_slice());
+
+        assert_eq!(py_masks.len()?, 0);
+
+        Ok(())
+    })
+}
+
+#[test]
 fn test_modelpack_split_u8() -> PyResult<()> {
     pyo3::append_to_inittab!(edgefirst_python);
     pyo3::prepare_freethreaded_python();
