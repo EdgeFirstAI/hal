9bca46b0288e4eec7f80cfd0fa9cbd8bc9276b53
Charlie-Zheng <charlie@au-zone.com>
2025-11-07 13:32:02 -0700

    EDGEAI-759 Adjusted python integration tests back to python crate

---
+++ tests/image/test_normalize.py
@@ -0,0 +1,14 @@
+import numpy as np
+from edgefirst_hal import TensorImage, ImageConverter,  FourCC, Normalization
+
+src = TensorImage.load(
+    "testdata/zidane.jpg", FourCC.RGBA)
+dst = TensorImage(640, 640, fourcc=FourCC.RGBA)
+converter = ImageConverter()
+converter.convert(src, dst)
+n = np.zeros((640, 640, 3), dtype=np.int8)
+dst.normalize_to_numpy(n, normalization=Normalization.SIGNED)
+unique_vals = list(np.unique(n))
+
+dst.normalize_to_numpy(n, normalization=Normalization.SIGNED, zero_point=127)
+unique_vals0 = list(np.unique(n))
