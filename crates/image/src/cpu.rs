6fa0aea43f87b506153b8d4bf7788c1b35859840
Charlie-Zheng <charlie@au-zone.com>
2025-12-03 12:35:03 -0700

    EDGEAI-988 Added more tests for image
    Signed-off-by: Charlie-Zheng <charlie@au-zone.com>

--- crates/image/src/cpu.rs
+++ crates/image/src/cpu.rs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2025 Au-Zone Technologies
 // SPDX-License-Identifier: Apache-2.0
 
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-
 use crate::{
     Crop, Error, Flip, FunctionTimer, GREY, ImageConverterTrait, NV12, NV16, PLANAR_RGB,
     PLANAR_RGBA, RGB, RGBA, Rect, Result, Rotation, TensorImage, YUYV,
