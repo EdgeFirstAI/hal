a84681181d85aa4db24cc02988281736f5762748
Charlie-Zheng <charlie@au-zone.com>
2026-03-13 17:02:10 -0600

    EDGEAI-1022 Added tracker feature, refactored internal code to reduce duplication

--- crates/tracker/src/lib.rs
+++ crates/tracker/src/lib.rs
@@ -1,20 +1,25 @@
 // SPDX-FileCopyrightText: Copyright 2025 Au-Zone Technologies
 // SPDX-License-Identifier: Apache-2.0
 
-use std::fmt::Debug;
+use std::{
+    cell::RefCell,
+    fmt::Debug,
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
 
-use uuid::Uuid;
-
+pub use uuid::Uuid;
 pub mod bytetrack;
+pub use bytetrack::{ByteTrack, ByteTrackBuilder};
 mod kalman;
 
-pub trait DetectionBox: Debug {
+pub trait DetectionBox: Debug + Clone {
     fn bbox(&self) -> [f32; 4];
     fn score(&self) -> f32;
     fn label(&self) -> usize;
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct TrackInfo {
     pub uuid: Uuid,
     pub tracked_location: [f32; 4],
@@ -23,8 +28,48 @@ pub struct TrackInfo {
     pub last_updated: u64,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ActiveTrackInfo<T: DetectionBox> {
+    pub info: TrackInfo,
+    // /// The last raw box associated with the track. This is before any smoothing
+    // /// introduced by the tracker
+    pub last_box: T,
+}
+
 pub trait Tracker<T: DetectionBox> {
     fn update(&mut self, boxes: &[T], timestamp: u64) -> Vec<Option<TrackInfo>>;
+    fn get_active_tracks(&self) -> Vec<ActiveTrackInfo<T>>;
+}
 
-    fn get_active_tracks(&self) -> Vec<TrackInfo>;
+pub trait TrackerGetActive {}
+
+impl<T, TR> Tracker<T> for Arc<Mutex<TR>>
+where
+    T: DetectionBox,
+    TR: Tracker<T>,
+{
+    fn update(&mut self, boxes: &[T], timestamp: u64) -> Vec<Option<TrackInfo>> {
+        let mut tracker = self.lock().unwrap();
+        tracker.update(boxes, timestamp)
+    }
+
+    fn get_active_tracks(&self) -> Vec<ActiveTrackInfo<T>> {
+        let tracker = self.lock().unwrap();
+        tracker.get_active_tracks()
+    }
+}
+
+impl<T, TR> Tracker<T> for Rc<RefCell<TR>>
+where
+    T: DetectionBox,
+    TR: Tracker<T>,
+{
+    fn update(&mut self, boxes: &[T], timestamp: u64) -> Vec<Option<TrackInfo>> {
+        let mut tracker = self.borrow_mut();
+        tracker.update(boxes, timestamp)
+    }
+    fn get_active_tracks(&self) -> Vec<ActiveTrackInfo<T>> {
+        let tracker = self.borrow();
+        tracker.get_active_tracks()
+    }
 }
